803de565ba5377110a67f911bd77919c1e503e17
saray-007 <s4r4y007@gmail.com>
2026-04-16 12:35:33 +0200

    Cambios en la App

--- AppSucursalBancaria.java
+++ AppSucursalBancaria.java
@@ -9,6 +9,7 @@ public class AppSucursalBancaria {
     private static Scanner entrada = new Scanner(System.in);
     private static ArrayList<Cliente> clientes = new ArrayList<>();
     private static ArrayList<Cuenta> cuentas = new ArrayList<>();
+    //cuenta seleccionada para operar con ella
     private static Cuenta cuentaActiva = null;
     private static int contadorCuenta = 1;
 
@@ -17,8 +18,7 @@ public class AppSucursalBancaria {
         int opcion = 0;
         while (opcion != 4) {
             menuPrincipal();
-            opcion = Integer.parseInt(entrada.nextLine());
-
+            opcion = Integer.parseInt(entrada.nextLine()); 
             switch (opcion) {
                 case 1:
                     crearCliente();
@@ -33,115 +33,100 @@ public class AppSucursalBancaria {
                     System.out.println("Fin del programa");
                     break;
                 default:
-                    System.out.println("Opción no válida");
+                    System.out.println("Opción no válida, selecciona una opción entre 1 y 4");
             }
         }
         entrada.close();
     }
 
     static void crearCliente() {
-        System.out.println("Dni:");
+
+        System.out.println("Dime el dni del cliente");
         String dni = entrada.nextLine();
-
-        System.out.println("Nombre:");
+        System.out.println("Dime el nombre del cliente");
         String nombre = entrada.nextLine();
-
-        System.out.println("Apellidos:");
+        System.out.println("Dime los apellidos del cliente");
         String apellidos = entrada.nextLine();
-
-        System.out.println("Dirección:");
+        System.out.println("Dime la dirección del cliente");
         String direccion = entrada.nextLine();
-
-        System.out.println("Localidad:");
-        String Localidad = entrada.nextLine();
-
-        System.out.println("Fecha nacimiento (AAAA-MM-DD):");
+        System.out.println("Dime la ciudad de residencia del cliente");
+        String ciudad = entrada.nextLine();
+        System.out.println("Dime la fecha de nacimiento del cliente AAAA-MM-DD");
         String fecha = entrada.nextLine();
 
-        clientes.add(new Cliente(dni,nombre, apellidos, direccion, Localidad, LocalDate.parse(fecha)));
-
-        System.out.println("Cliente creado correctamente"+"\n");
-        
+        clientes.add(new Cliente(dni,nombre, apellidos, direccion, ciudad, LocalDate.parse(fecha)));
+        System.out.println("El cliente de nombre "+nombre+" "+apellidos+" se ha creado correctamente");
     }
 
     static void crearCuenta() {
 
+        // si no hay clientes, no se puede crear cuenta
         if (clientes.isEmpty()) {
-            System.out.println("\nPrimero crea un cliente");
+            System.out.println("No hay clientes en la sucursal, primero debes crear uno");
             return;
         }
-
-        Cliente cliente;
-        do {
-            System.out.println("Selecciona el nombre del  cliente: ");
-            int i=0;
-            for (Cliente c : clientes) {
-                System.out.println(i+".-"+c.getNombre());
-                i++;
-            }
-
-            String nombre = entrada.nextLine();
-            cliente = validarCliente(nombre);
-
-        } while (cliente == null);
-
-        System.out.println("""
-                    ----------Tipo cuenta---------- 
-                    1- Cuenta Corriente 
-                    2- Cuenta Vivienda 
-                    3- Fondo Inversion
-                    """);
-        int tipo = Integer.parseInt(entrada.nextLine());
-
-        switch (tipo) {
-            case 1:
-                cuentas.add(new CuentaCorriente(contadorCuenta++, tipo, cliente));
-                break;
-
-            case 2:
-                cuentas.add(new CuentaVivienda(contadorCuenta++, tipo, cliente));
-                break;
-
-            case 3:
-                System.out.println("Interés:");
-                double interes = Double.parseDouble(entrada.nextLine());
-                cuentas.add(new FondoInversion(contadorCuenta++, tipo, cliente, interes));
-                break;
-
-            default:
-                System.out.println("Tipo no válido");
-        }
-
-        System.out.println("Cuenta creada correctamente"+"\n");
+        //obtener el cliente
+        Cliente cliente = validarCliente();
+        //obtener cuenta
+        Cuenta cuenta = validarCuenta(cliente);
+        //añadimos la cuenta al array de cuentas
+        cuentas.add(cuenta);
+        System.out.println("La cuenta a nombre de "+cliente.nombreCompleto()+" se ha creado correctamente");
     }
 
-    static Cliente validarCliente(String nombre) {
-        for (Cliente c : clientes) {
-            if (c.getNombre().equals(nombre)) {
-                return c;
+    static Cliente validarCliente() {
+        String dni;
+        while(true){
+            System.out.println("Dime el dni del cliente que quiere abrir una cuenta");
+            //mostrar la lista de los clientes
+            for (Cliente c : clientes) {
+                System.out.println(c.clienteCompleto());
+            }
+            dni = entrada.nextLine();
+            for (Cliente c : clientes) {
+                if (c.getDni().equals(dni)) {
+                    return c;
+                }
             }
         }
-        return null;
+    }        
+
+        
+    static Cuenta validarCuenta(Cliente cliente){
+        int opcion;
+        while(true){
+             menuTipoCuenta();
+             opcion = Integer.parseInt(entrada.nextLine());
+             switch (opcion) {
+                case 1:
+                    return new CuentaCorriente(contadorCuenta++, opcion, cliente);
+                case 2:
+                    return new CuentaVivienda(contadorCuenta++, opcion, cliente);
+                case 3:
+                    return new FondoInversion(contadorCuenta++, opcion, cliente, opcion);
+             }
+        }
     }
 
     static void seleccionarCuenta() {
 
+        // si no hay cuentas, mostramos mensaje
         if (cuentas.isEmpty()) {
-            System.out.println("No hay cuentas");
+            System.out.println("No hay ninguna cuenta en la sucursal, primero debes crear alguna");
             return;
         }
 
+        // seleccionamos una cuenta
         int numCuenta;
         do {
-            System.out.println("Selecciona cuenta:");
+            System.out.println("Selecciona un número de cuenta");
             for (Cuenta c : cuentas) {
-                System.out.println(c);
+                System.out.println(c.toString());
             }
-
-            numCuenta = Integer.parseInt(entrada.nextLine());
-
+            numCuenta = Integer.parseInt(entrada.nextLine());  
         } while (!validarCuenta(numCuenta));
 
+        // mostramos el menú de la cuenta
         seleccionarOpcionCuenta();
     }
 
@@ -157,13 +142,11 @@ public class AppSucursalBancaria {
 
     static void seleccionarOpcionCuenta() {
 
-        int opcion = 0;
-        while (opcion != 4) {
-
+        int opcionCuenta = 0;
+        while (opcionCuenta != 4) {
             menuCuenta();
-            opcion = Integer.parseInt(entrada.nextLine());
-
-            switch (opcion) {
+            opcionCuenta = Integer.parseInt(entrada.nextLine());  
+            switch (opcionCuenta) {
                 case 1:
                     ingresar();
                     break;
@@ -174,7 +157,7 @@ public class AppSucursalBancaria {
                     cuentaActiva.verDatos();
                     break;
                 case 4:
-                    System.out.println("Volviendo...");
+                    System.out.println("Volviendo al menú principal");
                     break;
                 default:
                     System.out.println("Opción no válida");
@@ -183,34 +166,54 @@ public class AppSucursalBancaria {
     }
 
     static void ingresar() {
-        System.out.println("Cantidad:");
-        double cantidad = Double.parseDouble(entrada.nextLine());
+        System.out.println("Indica la cantidad a ingresar");
+        int cantidad = Integer.parseInt(entrada.nextLine());        
         cuentaActiva.ingresar(cantidad);
     }
 
     static void retirar() {
-        System.out.println("Cantidad:");
-        double cantidad = Double.parseDouble(entrada.nextLine());
+        System.out.println("Indica la cantidad a retirar");
+        int cantidad = Integer.parseInt(entrada.nextLine());          
         cuentaActiva.retirar(cantidad);
     }
 
     static void menuPrincipal() {
-        System.out.println("""
-                ----------MENU PRINCIPAL----------
+
+        String s = """
+
+                MENU PRINCIPAL
                 1 - Crear Cliente
                 2 - Crear Cuenta
-                3 - Mantenimiento cuenta
+                3 - Mantenimiento de cuenta
                 4 - Salir
-                """);
+
+                """;
+        System.out.println(s);
     }
 
     static void menuCuenta() {
-        System.out.println("""
-                ----------MENU CUENTA----------
+
+        String s = """
+
+                MENU CUENTA
                 1 - Ingresar
                 2 - Retirar
-                3 - Ver datos
-                4 - Volver
-                """);
+                3 - Ver datos cuenta
+                4 - Volver al menú principal
+
+                """;
+        System.out.println(s);
+    }
+    static void menuTipoCuenta() {
+
+        String s = """
+
+                ELIJA EL TIPO CUENTA QUE QUIERE CREAR
+                1 - Cuenta Corriente
+                2 - Cuenta Vivienda
+                3 - Fondo Inversión
+
+                """;
+        System.out.println(s);
     }
 }
